971fe96b8d1700915395874bfb5cea18d6c876f4
Le Ngoc Thao <lnthao@tma.com.vn>
2017-05-05 14:21:30 +0700

    Add unit test for Writer and WriterRunner auto wiring

---
+++ src/test/java/spitter/web/services/WriterRunnerAutoWiredTest.java
@@ -0,0 +1,45 @@
+package spitter.web.services;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by lnthao on 5/5/2017.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=WriterRunner.class)
+public class WriterRunnerAutoWiredTest {
+    @Autowired
+    private IWriter writer;
+
+    @Autowired
+    private WriterRunner writerRunner;
+
+    @Test
+    public void writerShouldNotBeNull() {
+        assertNotNull(writerRunner.getWriter());
+    }
+
+    @Test
+    public void writerRunnerShouldNotBeNull() {
+        assertNotNull(writerRunner);
+    }
+
+    @Test
+    public void writerRunner_getWriter_isInstanceOfWriter(){
+        Assert.assertTrue(writerRunner.getWriter() instanceof Writer);
+    }
+
+    @Test
+    public void writerIsInjectedToRunnerSuccessfully(){
+        Assert.assertEquals(writer, writerRunner.getWriter());
+    }
+}
