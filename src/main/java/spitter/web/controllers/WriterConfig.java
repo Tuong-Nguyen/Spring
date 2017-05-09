4508759f94ca59f1b1f852faaf49b8fbb9e2ff72
trongtao <trongtao.gs@gmail.com>
2017-05-09 18:05:29 +0700

    Implement test function:
    WriterRunner run with NiceWriter (
      WriterRunner object must not be null
      IWriter object must not be null
      IWriter is an instance of Writer
      WriterRunner class's getWriter method return a writer is Writer )
    
    WriterRunner run with NiceWriter (
      WriterRunner object must not be null
      IWriter object must not be null
      IWriter is an instance of NiceWriter
      WriterRunner class's getWriter method return a writer is NiceWriter )
    
    (cherry picked from commit bd57827)

---
+++ src/main/java/spitter/web/controllers/WriterConfig.java
@@ -0,0 +1,27 @@
+package spitter.web.controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import spitter.web.services.Interfaces.IWriter;
+import spitter.web.services.NiceWriter;
+import spitter.web.services.Writer;
+import spitter.web.services.WriterRunner;
+import spitter.web.services.WriterRunner;
+
+/**
+ * Created by nttao on 5/9/2017.
+ */
+@Configuration
+@Import({Writer.class, WriterRunner.class})
+public class WriterConfig {
+//    @Bean
+//    public IWriter writer(){
+//        return  new Writer();
+//    }
+//    @Bean
+//    public WriterRunner1 runner(){
+//        return new WriterRunner1();
+//    }
+
+}
