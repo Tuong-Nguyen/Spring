de24e1662d1b607e491b0b39d02fb8407106fa95
Le Ngoc Thao <lnthao@tma.com.vn>
2017-05-11 16:15:58 +0700

    XmlViewResolver configuration

--- src/main/java/spitter/config/WebConfig.java
+++ src/main/java/spitter/config/WebConfig.java
@@ -1,13 +1,17 @@
 package spitter.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.XmlViewResolver;
+import org.springframework.core.io.Resource;
 
 /**
  * Created by lnthao on 4/28/2017.
@@ -23,6 +27,22 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    @Bean
+    public ViewResolver xmlViewResolverDefault() {
+        XmlViewResolver resolver = new XmlViewResolver();
+        resolver.setOrder(1);
+        return resolver;
+    }
+
+    @Bean
+    public ViewResolver xmlViewResolver() {
+        XmlViewResolver resolver = new XmlViewResolver();
+        Resource resource = new ClassPathResource("/views.xml"); // src/main/resources/views.xml
+        resolver.setLocation(resource);
+        resolver.setOrder(2);
         return resolver;
     }
 
