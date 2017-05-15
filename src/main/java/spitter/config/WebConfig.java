1e33f2bba3ddf0655d4d589a2c264287123b4e0b
Le Ngoc Thao <lnthao@tma.com.vn>
2017-05-15 14:27:52 +0700

    Override configureContentNegotiation method, add content negotiating view resolver bean, add json view resolver bean

--- src/main/java/spitter/config/WebConfig.java
+++ src/main/java/spitter/config/WebConfig.java
@@ -1,17 +1,21 @@
 package spitter.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.XmlViewResolver;
 import org.springframework.core.io.Resource;
+import spitter.config.views.JsonViewResolver;
 
 /**
  * Created by lnthao on 4/28/2017.
@@ -20,6 +24,22 @@ import org.springframework.core.io.Resource;
 @EnableWebMvc
 @ComponentScan("spitter.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureContentNegotiation
+            (ContentNegotiationConfigurer configurer) {
+        configurer
+                .defaultContentType(MediaType.TEXT_HTML)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("json", MediaType.APPLICATION_JSON);
+    }
+
+    @Bean
+    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager){
+        ContentNegotiatingViewResolver cnViewResolver = new ContentNegotiatingViewResolver();
+        cnViewResolver.setContentNegotiationManager(manager);
+        return cnViewResolver;
+    }
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver =
@@ -43,6 +63,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         Resource resource = new ClassPathResource("/views.xml"); // src/main/resources/views.xml
         resolver.setLocation(resource);
         resolver.setOrder(2);
+        return resolver;
+    }
+
+    @Bean
+    public ViewResolver jsonViewResolver(){
+        JsonViewResolver resolver = new JsonViewResolver();
         return resolver;
     }
 
