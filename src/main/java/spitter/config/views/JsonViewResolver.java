e36712d972af7f7382ad5237c209805aadaa3c17
Le Ngoc Thao <lnthao@tma.com.vn>
2017-05-15 14:25:48 +0700

    Add json view resolver

---
+++ src/main/java/spitter/config/views/JsonViewResolver.java
@@ -0,0 +1,21 @@
+package spitter.config.views;
+
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.Locale;
+
+/**
+ * Created by lnthao on 5/12/2017.
+ */
+public class JsonViewResolver implements ViewResolver {
+    @Override
+    public View resolveViewName(String viewName, Locale locale)
+            throws Exception {
+        MappingJackson2JsonView view = new MappingJackson2JsonView();
+        view.setPrettyPrint(true);
+        return view;
+    }
+
+}
